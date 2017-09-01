a49f4de5d842e0343f151c7648d0921fd2823597
Kirill Holodyuk <kirill.holodyuk@gmail.com>
2017-09-02 00:01:56 +0300

    code review

--- js/adunits.js
+++ js/adunits.js
@@ -51,7 +51,7 @@ AdUnitController = (function () {
                         console.log(message);
                     });
                 } else {
-                    message = "Something went wrong. Please contact Appodeal support.";
+                    message = "Something went wrong. Please contact " + wl_projectName + " support.";
                     modal.show(modal_header, message);
                     sendOut(0, message);
                     throw new Error(message);
@@ -92,70 +92,3 @@ AdUnitController = (function () {
 $(document).ready(function () {
     AdUnitController.init();
 });
-
-
-// chrome.storage.local.get("admob_processing", function (result) {
-//     if (result['admob_processing']) {
-//         console.log("Start admob adunits processing");
-//         setTimeout(function () {
-//             startInventorySync();
-//         }, 4000);
-//     }
-// });
-//
-// function startInventorySync() {
-//     console.log("startInventorySync");
-//     appendJQuery(function () {
-//         // get api key and user id from storage and sync inventory
-//         chrome.storage.local.get({
-//             'appodeal_api_key': null,
-//             'appodeal_user_id': null,
-//             'appodeal_admob_account_publisher_id': null,
-//             'appodeal_admob_account_email': null,
-//             'accounts': null,
-//             'interstitialBids': null,
-//             'bannerBids': null,
-//             'mrecBids': null,
-//             'rewarded_videoBids': null
-//         }, function (items) {
-//             if (items['appodeal_api_key'] && items['appodeal_user_id'] && items['appodeal_admob_account_publisher_id']) {
-//                 criticalUpdates(function (updates) {
-//                     var admob = null;
-//                     if (window.location.href.match(/apps\.admob\.com\/v2/)) {
-//                         //New version Admob from 18.05.2017
-//                         admob = new AdmobV2(
-//                             items['appodeal_user_id'],
-//                             items['appodeal_api_key'],
-//                             items['appodeal_admob_account_publisher_id'],
-//                             items['appodeal_admob_account_email'],
-//                             items['accounts'],
-//                             items['interstitialBids'],
-//                             items['bannerBids'],
-//                             items['mrecBids'],
-//                             items['rewarded_videoBids']
-//                         );
-//                     } else {
-//                         //Old version Admob
-//                         admob = new Admob(
-//                             items['appodeal_user_id'],
-//                             items['appodeal_api_key'],
-//                             items['appodeal_admob_account_publisher_id'],
-//                             items['appodeal_admob_account_email'],
-//                             items['accounts'],
-//                             items['interstitialBids'],
-//                             items['bannerBids'],
-//                             items['mrecBids'],
-//                             items['rewarded_videoBids']
-//                         );
-//                     }
-//                     admob.syncInventory(function () {
-//                         console.log("Apps and adunits have been synced successfully.");
-//                     });
-//                 })
-//             } else {
-//                 modal = new Modal();
-//                 modal.show("Appodeal Chrome Extension", "Something went wrong. Please contact Appodeal support.");
-//             }
-//         })
-//     });
-// }
